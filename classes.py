2c8d0f2be0b437b6b2e3e23f7b5f0abfe954718e
antunsimic <95712724+antunsimic@users.noreply.github.com>
2024-06-06 21:55:33 +0200

    Update classes.py
    dodavanje funkcionalnosti u skladu s dijagramom slijeda

--- classes.py
+++ classes.py
@@ -1,4 +1,3 @@
-
 from enum import Enum
 
 class Stanje(Enum):
@@ -20,8 +19,15 @@ class PrimarnaOprema:
     def __init__(self, stanje):
         self.stanje = stanje
 
-    def komanda(self, ukljuci):
-        pass
+    def komanda(self, ukljuci): 
+        #ukljuci = true -> uključenje 
+        #ukljuci = false -> isključenje
+        self.stanje=ukljuci
+        return self.stanje
+    
+    def odredi_polozaj(self):
+        return self.stanje
+    
 
 class Prekidac(PrimarnaOprema):
     pass
@@ -40,44 +46,46 @@ class Polje:
 
 
 class PrekidacLTB145D1(Prekidac):
-    def __init__(self, stanje, gubitak_sf6, blokada_rada, blokada_isklopa, opruga_navijena):
+    def __init__(self, stanje):
         super().__init__(stanje)
-        self.gubitak_sf6 = gubitak_sf6
-        self.blokada_rada = blokada_rada
-        self.blokada_isklopa = blokada_isklopa
-        self.opruga_navijena = opruga_navijena
+        self.gubitak_sf6 = StanjeOp.PRESTANAK
+        self.blokada_rada = StanjeOp.PRESTANAK
+        self.blokada_isklopa = StanjeOp.PRESTANAK
+        self.opruga_navijena = StanjeOp.PRESTANAK
+    
+    def provjeriGubitakSF6(self):
+        return self.gubitak_sf6
 
 class APU():
-    def __init__(self, ukljucenje, p1, p3, blokada):
-        self.ukljucenje = ukljucenje
-        self.p1 = p1
-        self.p3 = p3
-        self.blokada = blokada
-
+    def __init__(self):
+        self.ukljucenje = StanjeOp.PRESTANAK
+        self.p1 = StanjeOp.PRESTANAK
+        self.p3 = StanjeOp.PRESTANAK
+        self.blokada = StanjeOp.PRESTANAK
+        
+        
 class Distantna(Zastita):
-    def __init__(self, iskljucenje, faza_l1, faza_l2, faza_l3, zemljospoj, kvar):
-        self.iskljucenje = iskljucenje
-        self.faza_l1 = faza_l1
-        self.faza_l2 = faza_l2
-        self.faza_l3 = faza_l3
-        self.zemljospoj = zemljospoj
-        self.kvar = kvar
+    def __init__(self):
+        self.iskljucenje = StanjeOp.PRESTANAK
+        self.faza_l1 = StanjeOp.PRESTANAK
+        self.faza_l2 = StanjeOp.PRESTANAK
+        self.faza_l3 = StanjeOp.PRESTANAK
+        self.zemljospoj = StanjeOp.PRESTANAK
+        self.kvar = StanjeOp.PRESTANAK
 
 class Nadstrujna(Zastita):
-    def __init__(self, npc_iskljucenje, vpc_iskljucenje, zemljospojna_iskljucenje, od_preopterecenja_upozorenje, od_preopterecenja_iskljucenje, relej_kvar):
-        self.npc_iskljucenje = npc_iskljucenje
-        self.vpc_iskljucenje = vpc_iskljucenje
-        self.zemljospojna_iskljucenje = zemljospojna_iskljucenje
-        self.od_preopterecenja_upozorenje = od_preopterecenja_upozorenje
-        self.od_preopterecenja_iskljucenje = od_preopterecenja_iskljucenje
-        self.relej_kvar = relej_kvar
+    def __init__(self):
+        self.npc_iskljucenje = StanjeOp.PRESTANAK
+        self.vpc_iskljucenje = StanjeOp.PRESTANAK
+        self.zemljospojna_iskljucenje = StanjeOp.PRESTANAK
+        self.od_preopterecenja_upozorenje = StanjeOp.PRESTANAK
+        self.od_preopterecenja_iskljucenje = StanjeOp.PRESTANAK
+        self.relej_kvar = StanjeOp.PRESTANAK
 
 class MjerniPretvornik:
     def __init__(self, radna_energija, jalova_snaga):
         self.radna_energija = radna_energija
         self.jalova_snaga = jalova_snaga
-
-
 
 
 
@@ -87,6 +95,7 @@ class RSabirnicki(Rastavljac):
 class RIzlazni(Rastavljac):
     pass  
 
+
 class DalekovodnoPolje(Polje):
     def __init__(self, prekidac, s_rastavljac, i_rastavljac, dist_zastita, nads_zastita, apu, mjera):
         super().__init__(prekidac, s_rastavljac, i_rastavljac)
@@ -95,8 +104,8 @@ class DalekovodnoPolje(Polje):
         self.apu = apu
         self.mjera = mjera
         self.grupni_iskljucenje = StanjeOp.PRESTANAK  
-        self.grupni_upozorenje = StanjeOp.PRORADA
-        self.grupni_smetnje = StanjeOp.PRORADA
+        self.grupni_upozorenje = StanjeOp.PRESTANAK
+        self.grupni_smetnje = StanjeOp.PRESTANAK
 
 class Napajanje:
     def __init__(self, stanje, kapacitet, napon, potrosnja):
@@ -105,14 +114,22 @@ class Napajanje:
         self.napon = napon
         self.potrosnja = potrosnja
 
-class TPKoncar(Rastavljac):
-    def __init__(self):
-        self.pad_tlaka_16b = StanjeOp.PRORADA
-        self.pad_tlaka_14b = StanjeOp.PRORADA
-        self.pad_tlaka_11b = StanjeOp.PRORADA
-        self.apu_blokada = StanjeOp.PRORADA
-        self.nesklad_polova_3p_isklop = StanjeOp.PRORADA
-        self.upravljanje = Upravljanje.DALJINSKO
+class TPKoncar(Prekidac):
+    def __init__(self, stanje, upravljanje, tlak):
+        super().__init__(stanje)
+        self.tlak = tlak
+        self.pad_tlaka_16b = StanjeOp.PRESTANAK
+        self.pad_tlaka_14b = StanjeOp.PRESTANAK
+        self.pad_tlaka_11b = StanjeOp.PRESTANAK
+        self.apu_blokada = StanjeOp.PRESTANAK
+        self.nesklad_polova_3p_isklop = StanjeOp.PRESTANAK
+        self.upravljanje = upravljanje
+    
+    def odrediTlak(self):
+        return self.tlak
+    
+    def odrediUpravljanje(self):
+        return self.upravljanje
 
 class Dalekovod:
     def ukljuci(self):
@@ -125,6 +142,5 @@ class SpojnoPolje(Polje):
     def __init__(self, prekidac, s_rastavljac, i_rastavljac):
         super().__init__(prekidac, s_rastavljac, i_rastavljac)
         self.grupni_iskljucenje = StanjeOp.PRESTANAK
-        self.grupni_upozorenje = StanjeOp.PRORADA
-        self.grupni_smetnje = StanjeOp.PRORADA
-
+        self.grupni_upozorenje = StanjeOp.PRESTANAK
+        self.grupni_smetnje = StanjeOp.PRESTANAK
